fc422a09837d7de9b36253a622c8b8a7481e5196
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-03-30 00:10:42 +0700

    update:Add->TestCrawlData

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,8 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
+import axios from "axios";
+import * as cheerio from 'cheerio';
+import cron from "node-cron";
 
 // Initialized configuration
 const app: Express = express();
@@ -7,6 +10,26 @@ const app: Express = express();
 // use configuration
 app.use(express.json());
 app.use(cors()); 
+
+// Crawler configuration
+let crawledData: string = "";
+
+const crawlData = async () => {
+  try {
+    const res = await axios.get("https://ou.edu.vn/thong-bao-cho-sinh-vien/");
+    const $ = cheerio.load(res.data);
+    crawledData = $(".wpb_wrapper").text();
+    console.log("Data crawled successfully:", crawledData);
+  } catch (error: any) {
+    console.error("Error occurred while crawling data:", error);
+  }
+};
+
+cron.schedule("* * * * *", crawlData);
+
+app.get("/api/v1/data", (req: Request, res: Response) => {
+  res.json({ data: crawledData });
+});
 
 // Handle Logic
 app.get("/", (req: Request, res: Response) => {
