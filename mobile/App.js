61a2c058d384f578067043b1b413217bdb3eb549
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-03-29 22:48:22 +0700

    update:Add->TestAPI

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:4000');
+      const json = await response.json();
+      setData(json.message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{data}</Text>
       <StatusBar style="auto" />
     </View>
   );
