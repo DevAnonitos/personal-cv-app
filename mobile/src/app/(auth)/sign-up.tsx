914ba47af56024b9b9a452481067fd7047e46981
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-03 21:54:54 +0700

    update:Add->ConfigComponentAuth

--- mobile/src/app/(auth)/sign-up.tsx
+++ mobile/src/app/(auth)/sign-up.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, Stack } from 'expo-router';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 
 const SignUpScreen = () => {
   return (
