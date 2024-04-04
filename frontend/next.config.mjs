0616075871a0f41566cabba2c4ea0a23614d9c78
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-04 21:52:18 +0700

    update:Add->NextJsConfig

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    mdxRs: true,
+  },
+  pageExtensions: ['ts', 'tsx', 'mdx'],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    }
+  }
+};
 
 export default nextConfig;
