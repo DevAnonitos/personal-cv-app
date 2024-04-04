89594f5c7a6ff211c61b2520e9782acc1f9b79e7
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-04 23:57:03 +0700

    update:Add->AssetsToNavbar

--- frontend/components/shared/TopBar.tsx
+++ frontend/components/shared/TopBar.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const TopBar = () => {
   return (
-    <div className='topBar'>
-      <div className='flex items-center p-4 h-full'>
+    <nav className='topBar'>
+      <div className='flex items-center justify-between gap-x-4 p-4 w-full h-full '>
+        <Link href="/" className='flex gap-2 items-center justify-center'>
+          <Image 
+            src="./assets/images/logo.svg"
+            alt='logo'
+            width={40}
+            height={40}
+            className='object-contain'
+          />
+          <p>DevAnonitos</p>
+        </Link>
 
+        <div>
+          Search
+        </div>
+
+        <div>
+          profile
+        </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
