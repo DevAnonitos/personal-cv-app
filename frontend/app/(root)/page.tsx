8cf4033bd85ebca4ef9c502a0b542d7eb414fac3
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-05 11:07:42 +0700

    update:Add->Badge

--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex items-center h-full p-6">
+    <main className="flex flex-col w-full h-full p-6">
       <h1>
         MainPage  
       </h1>
