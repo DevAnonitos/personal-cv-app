f833c858065e39082bac6c2aec4ea5d977b0ddf9
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-05 20:34:39 +0700

    update:Add->SocialMediaLinks

--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -5,3 +5,21 @@ export const leftSideBarLinks = [
 export const bottomBarLinks = [
   
 ];
+
+export const socialMediaLinks = [
+  {
+    imgUrl: "/assets/icons/facebook.svg",
+    linkUrl: "",
+    label: "Facebook",
+  },
+  {
+    imgUrl: "/assets/icons/instagram.svg",
+    linkUrl: "",
+    label: "Instagram",
+  },
+  {
+    imgUrl: "/assets/icons/tiktok.svg",
+    linkUrl: "",
+    label: "Tiktok",
+  },
+];
