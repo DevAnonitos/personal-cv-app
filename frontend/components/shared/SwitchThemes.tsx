ee3e7eb7841cdad98cc8425b8e8298cf5cae211e
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-07 23:29:01 +0700

    update:Add->SwitchThemesComponents

---
+++ frontend/components/shared/SwitchThemes.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
+
+const SwitchThemes = () => {
+  return (
+    <div>SwitchThemes</div>
+  );
+};
+
+export default SwitchThemes;
