ed9f36dc0e6d8d583e4a5f35e7a5a5c0ac8ce636
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-09 00:29:21 +0700

    update:Add->LoadingPage

---
+++ frontend/app/(root)/blog/loading.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Loading () {
+  return (
+    <div className="flex items-center justify-center">
+      <p>Loading ...</p>
+    </div>
+    
+  );
+};
--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -4,10 +4,11 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <section className="flex flex-col w-full h-full p-6">
-      <h1>
-        MainPage  
-      </h1>
+    <section className="flex flex-col w-full h-full p-6 space-y-4">
+      {/* List of TagButtonName */}
+      <div className="flex items-center gap-x-2 overflow-x-auto pb-2">
+        
+      </div>
     </section>
   );
 };
