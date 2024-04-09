5a3cfc30f398c5dcf13571bf0b5e7f434d02c3f4
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-09 23:56:10 +0700

    update:Add->Axios&LoadingPage

--- frontend/app/(root)/loading.tsx
+++ frontend/app/(root)/loading.tsx
@@ -1,8 +1,17 @@
 "use client";
+import React from "react";
+import Image from "next/image";
 
 export default function Loading () {
   return (
-    <div className="flex items-center justify-center">
+    <div className="w-full flex items-center justify-center">
+      <Image
+        src="/assets/icons/loader.svg"
+        alt="Loader"
+        width={50}
+        height={50}
+        className="object-contain animate-spin"
+      />
       <p>Loading ...</p>
     </div>
     
