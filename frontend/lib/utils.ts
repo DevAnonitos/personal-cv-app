9c1f43990ec95d36c122ca5b7d2c2fda410aa18a
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-09 21:44:35 +0700

    update:Add->InitFormatDateString

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import moment from "moment";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
+};
+
+export function formatDateString (dateString: string) {
+
+};
