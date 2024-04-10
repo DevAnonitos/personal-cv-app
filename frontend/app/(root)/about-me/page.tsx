76c705efef73a89bb510d61f706b83c83ffa2ebc
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-10 23:55:24 +0700

    update:Add->BannerAboutMe

--- frontend/app/(root)/about-me/page.tsx
+++ frontend/app/(root)/about-me/page.tsx
@@ -7,9 +7,28 @@ const Page = () => {
       {/* Banner AboutMe */}
       <div className='bannerAboutMeImage'>
         {/* Image Thumb Cover */}
+        <Image
+          src={"/assets/facebook.svg"}
+          width={150}
+          height={150}
+          alt='Loading'
+          className='object-contain absolute t-[50px]'
+        />
       </div>
-      <div className='flex justify-center'>
+        
+      <div className='flex w-full flex-col items-center justify-center'>
         {/* Content */}
+        <h2 className='text-3xl font-bold'>
+          About me
+        </h2>
+        <div className='flex w-full flex-row bg-slate-200 mt-4 max-md:flex-col'>
+          <div className='w-1/2 max-md:w-full bg-red-300'>
+            h1
+          </div>
+          <div className='w-1/2 max-md:w-full bg-blue-500'>
+            h2
+          </div>
+        </div>
       </div>
     </section>
   );
