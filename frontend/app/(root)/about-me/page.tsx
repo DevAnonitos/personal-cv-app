cb362047dc7a44f47ea7539675055f309addff07
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-11 22:36:17 +0700

    update:Add->BannerAnimate

--- frontend/app/(root)/about-me/page.tsx
+++ frontend/app/(root)/about-me/page.tsx
@@ -12,19 +12,26 @@ const Page = () => {
           height={150}
           alt='Loading'
           loading='lazy'
-          className='object-contain rounded-full border 
-          border-slate-600 max-md:w-[120px] max-md:h-[120px]'
+          className='object-contain rounded-full 
+          max-md:w-[120px] max-md:h-[120px]'
         />
       </div>
-        
+      {/* Content About Me */}
       <div className='flex w-full flex-col items-center justify-center'>
         {/* Content */}
-        <h2 className='text-3xl font-bold'>
+        <h2 className='text-3xl font-bold max-md:text-xl'>
           About me
         </h2>
-        <div className='flex w-full flex-row bg-slate-200 mt-4 max-md:flex-col'>
-          <div className='w-1/2 max-md:w-full bg-red-300'>
-            h1
+        <div className='flex w-full flex-row mt-4 max-md:flex-col'>
+          <div className='w-1/2 flex items-center justify-center max-md:w-full'>
+            <Image 
+              src="/assets/images/study.gif"
+              alt='Study'
+              width={350}
+              height={350}
+              className='object-contain max-md:w-[150px] max-md:h-[150px]'
+            />
+            {/* Social Icons */}
           </div>
           <div className='w-1/2 max-md:w-full bg-blue-500'>
             h2
