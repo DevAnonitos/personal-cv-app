e67391cf91d2b5a9f749fcdef8cb66748ee6f7a9
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-14 00:12:07 +0700

    update:Add->ConfigModulesTypes

--- mobile/constants/icons.ts
+++ mobile/constants/icons.ts
@@ -1,17 +1,16 @@
-// @ts-ignore
-const bookmark =  require("../assets/icons/bookmark.png");
-const home =  require("../assets/icons/home.png");
-const plus =  require("../assets/icons/plus.png");
-const profile = require("../assets/icons/profile.png");
-const leftArrow =  require("../assets/icons/left-arrow.png");
-const menu = require("../assets/icons/menu.png");
-const search =  require("../assets/icons/search.png");
-const upload = require("../assets/icons/upload.png");
-const rightArrow = require("../assets/icons/right-arrow.png");
-const logout = require("../assets/icons/logout.png");
-const eyeHide = require("../assets/icons/eye-hide.png");
-const eye = require("../assets/icons/eye.png");
-const play = require("../assets/icons/play.png");
+import bookmark from "../assets/icons/bookmark.png";
+import home from "../assets/icons/home.png";
+import plus from "../assets/icons/plus.png";
+import profile from "../assets/icons/profile.png";
+import leftArrow from "../assets/icons/left-arrow.png";
+import menu from "../assets/icons/menu.png";
+import search from "../assets/icons/search.png";
+import upload from "../assets/icons/upload.png";
+import rightArrow from "../assets/icons/right-arrow.png";
+import logout from "../assets/icons/logout.png";
+import eyeHide from "../assets/icons/eye-hide.png";
+import eye from "../assets/icons/eye.png";
+import play from "../assets/icons/play.png";
 
 export default {
   play,
