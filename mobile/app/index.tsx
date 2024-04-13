19f3b704b5bfba3bd7470be2f9de5fd627a252fb
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-13 13:11:49 +0700

    update:Add->ConfigApp

---
+++ mobile/app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+const WelCome = () => {
+  return (
+    <View className='bg-blue-500'>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  )
+}
+
+export default WelCome
