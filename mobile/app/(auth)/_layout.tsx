f3f2ad1ce0352fdcc5a2fca4f1b580af19d572e2
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-13 15:09:04 +0700

    update:Add->AuthLayout

---
+++ mobile/app/(auth)/_layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Redirect, Stack } from "expo-router"
+import { StatusBar } from 'expo-status-bar';
+
+const AuthLayout = () => {
+  return (
+    <>
+      <Stack>
+
+      </Stack>
+
+      <StatusBar backgroundColor='#161622' style='light' />
+    </>
+  );
+};
+
+export default AuthLayout;
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -17,9 +17,16 @@ export const unstable_settings = {
 };
 
 const RootLayout = () => {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
   });
 
   useEffect(() => {
@@ -27,12 +34,16 @@ const RootLayout = () => {
   }, [error]);
 
   useEffect(() => {
-    if (loaded) {
+    if (fontsLoaded) {
       SplashScreen.hideAsync();
     }
-  }, [loaded]);
+  }, [fontsLoaded]);
 
-  if (!loaded) {
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  if(!fontsLoaded && !error) {
     return null;
   }
 
