c3fd92bac93fe68452f03b127faafe24b4a98463
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-16 22:43:36 +0700

    update:Add->ReduxToolKit-Redux

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -45,7 +45,7 @@ const RootLayout = () => {
         <Stack.Screen name='(auth)' options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name='index' options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+
       </Stack>
     </ThemeProvider>
   );
