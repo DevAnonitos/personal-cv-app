2b61a2eb0ade9c06beb8b304a02a406107e93a20
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-17 23:44:25 +0700

    update:Add->ProfileTab

---
+++ mobile/app/(tabs)/profile.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { router } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Image, View, FlatList, TouchableOpacity } from 'react-native';
+
+import { icons } from '@/constants';
+
+
+const Profile = () => {
+  return (
+    <div>Profile</div>
+  );
+};
+
+export default Profile;
