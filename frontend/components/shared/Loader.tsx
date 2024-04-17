1ca1558a20b4aaf98f8cd7de625cd4f8c23ef036
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-17 22:30:48 +0700

    update:Add->LoaderComponents

--- frontend/components/shared/Loader.tsx
+++ frontend/components/shared/Loader.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <div className='flex items-center justify-center w-full h-full'>
+      <Image
+        src="/assets/icons/loader.svg"
+        width={44}
+        height={44}
+        alt='Loader'
+        className='object-contain'
+      />
+    </div>
   );
 };
 
