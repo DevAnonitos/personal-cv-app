1e7922205b2570cca97560d7c6157cae9a20b2fd
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-18 00:24:01 +0700

    update:Add->CreateScreen

---
+++ mobile/app/(tabs)/create.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { router } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+
+
+const Create = () => {
+  return (
+    <div>Create</div>
+  );
+};
+
+export default Create;
