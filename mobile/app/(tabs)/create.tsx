1a5a1149e66762b5c0402f6828bf1ee0f8b37659
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-18 09:01:01 +0700

    update:Add->Create

--- mobile/app/(tabs)/create.tsx
+++ mobile/app/(tabs)/create.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
+import { 
+  ScrollView, 
+  Text, 
+  Alert, 
+  Image, 
+  TouchableOpacity 
+} from 'react-native';
 
 
 const Create = () => {
   return (
-    <div>Create</div>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView>
+
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
