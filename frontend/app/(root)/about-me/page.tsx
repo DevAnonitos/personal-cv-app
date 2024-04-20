2d17b3a44cc7d6de0d3001ab3739817e37f2e333
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-20 13:57:24 +0700

    update:Add->Remove About Page

--- frontend/app/(root)/about-me/page.tsx
+++ frontend/app/(root)/about-me/page.tsx
@@ -7,43 +7,7 @@ import { socialLinks } from '@/constants';
 const Page = () => {
   return (
     <section className='container_page'>
-      {/* Banner AboutMe */}
-      <div className='bannerAboutMeImage'>
-        <Image
-          src={"/assets/images/logo.jpg"}
-          width={150}
-          height={150}
-          alt='Loading'
-          loading='lazy'
-          className='object-contain rounded-full 
-          max-md:w-[120px] max-md:h-[120px]'
-        />
-      </div>
-      {/* Content About Me */}
-      <div className='flex w-full flex-col items-center justify-center'>
-        {/* Content */}
-        <h2 className='text-3xl font-bold max-md:text-xl'>
-          About me
-        </h2>
-        <div className='flex w-full flex-row mt-4 max-md:flex-col'>
-          <div className='w-1/2 flex items-center justify-center max-md:w-full'>
-            <Image 
-              src="/assets/images/study.gif"
-              alt='Study'
-              width={350}
-              height={350}
-              unoptimized
-              className='object-contain max-md:w-[185px] max-md:h-[185px]'
-            />
-          </div>
-          <div className='w-1/2 max-md:w-full border border-slate-300 rounded-xl'>
-            h2
-            <p>
-
-            </p>
-          </div>
-        </div>
-      </div>
+      
     </section>
   );
 };
