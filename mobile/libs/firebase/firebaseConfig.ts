2148071891d43b6dd49a8f64ac2fea7a68682e20
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-04-23 11:48:39 +0700

    update:Add->Example Conf Firebase

---
+++ mobile/libs/firebase/firebaseConfig.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: 'api-key',
+  authDomain: 'project-id.firebaseapp.com',
+  databaseURL: 'https://project-id.firebaseio.com',
+  projectId: 'project-id',
+  storageBucket: 'project-id.appspot.com',
+  messagingSenderId: 'sender-id',
+  appId: 'app-id',
+  measurementId: 'G-measurement-id',
+};
+
+const app = initializeApp(firebaseConfig);
