1d34c14ddcf7f444f4f8e173f761545ab25f885e
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-05-09 22:33:08 +0700

    update:Add->SwitchThemes

---
+++ mobile/components/SwitchThemes.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SwitchThemes = () => {
+  return (
+    <div>SwitchThemes</div>
+  );
+};
+
+export default SwitchThemes;
--- mobile/components/index.ts
+++ mobile/components/index.ts
@@ -1,9 +1,11 @@
 import CustomButton from "./CustomButton";
 import Loader from "./Loader";
 import FormField from "./FormField";
+import SwitchThemes from "./SwitchThemes";
 
 export {
   CustomButton,
   Loader,
   FormField,
+  SwitchThemes,
 };
