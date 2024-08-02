b123753611c6e0cdef35e372f479c0dc0d5ed55f
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-08-02 21:35:05 +0700

    update:Add->AdminRoutes

---
+++ frontend/app/admin/layout.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "../../styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  generator: "Next.js",
+  applicationName: "PortfolioAppCV",
+  title: "DevAnonitos Portfolio",
+  keywords: ['Next.js', 'React', 'TypeScript'],
+  description: "Build a project profile to CV",
+  category: "Development",
+  creator: "DevAnonitos",
+  authors: [{ name: "BaoNguyen" }]
+};
+
+export default function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  );
+};
